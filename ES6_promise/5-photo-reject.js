78f02c70b06dc8d185c2b7cd4f04d1d23a7be4dd
Aldion <aldion@gmail.com>
2025-08-04 17:02:30 +0200

    task 6 completed

---
+++ ES6_promise/5-photo-reject.js
@@ -0,0 +1,9 @@
+export default function uploadPhoto(filename) {
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+}
+
+// export default function uploadPhoto(filename) {
+//   return new Promise((resolve, reject) => {
+//     reject new Error(`${filename} cannot be processed`)
+//   }
+// }

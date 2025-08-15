ca0e848084172f680130da855e494bf7f507ae54
Aldion <aldion@gmail.com>
2025-08-15 14:07:19 +0200

    ES6_classes updated lines

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
 export default class Airport {
-    construstor(name, code) {
-      this._name = name;
-      this._code = code;
-    }
-  
-    toString() {
-      return `[object ${this._code}]`;
-    }
+  construstor(name, code) {
+    this._name = name;
+    this._code = code;
   }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}

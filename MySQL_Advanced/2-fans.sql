650cdee76f15038e951cbe18cf75e7105b8b7ef4
Aldion <aldion@gmail.com>
2025-09-11 15:07:58 +0200

    condition on task 2 removed

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,7 +1,6 @@
 -- Write a SQL script that ranks country origins of bands,
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
-WHERE origin IN ('USA', 'Sweden', 'Finland', 'United Kingdom', 'Germany', 'Norway', 'Canada', 'The Netherlands', 'Italy')
 GROUP BY origin
 ORDER BY nb_fans DESC;
   

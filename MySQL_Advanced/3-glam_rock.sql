ecd2e307b6f42e427eae2beffa9205dee009fadf
Aldion <aldion@gmail.com>
2025-09-21 11:16:12 +0200

    task 3 updated

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,13 @@
 -- Write a SQL script that lists all bands with Glam rock
-SELECT
-  band_name,
+SELECT band_name,
   CASE
     WHEN split IS NULL OR split = 0 THEN YEAR(CURDATE()) - formed
     ELSE split - formed
   END AS lifespan
+
 FROM metal_bands
+
 WHERE style LIKE '%Glam rock%'
   AND formed IS NOT NULL
+  
 ORDER BY lifespan DESC, band_name ASC;
